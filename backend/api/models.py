068ac9a2e66ca4a666774d66ef11083104040ffb
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:08:47 +0300

    created company file model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class CompanyProfile(models.Model):
+    name = models.CharField(max_length=200)
+    hero_title = models.CharField(max_length=200)
+    hero_subtitle = models.TextField()
+    contact_email = models.EmailField()
+    logo = models.ImageField(upload_to='assets/')
+
+    def __str__(self):
+        return self.name
