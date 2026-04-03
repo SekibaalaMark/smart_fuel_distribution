42906fed93b12992a3e55bcd8af0774f63fcc48b
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:36:15 +0300

    created Company Info View

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,21 @@
-# views.py
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from django.core.mail import send_mail
-from .serializers import ContactInquirySerializer
+from .models import CompanyProfile, ContactInquiry
+from .serializers import CompanyProfileSerializer, ContactInquirySerializer
 
-class ContactCreateView(APIView):
-    def post(self, request):
-        serializer = ContactInquirySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            
-            # Send Email
-            send_mail(
-                subject=f"New Inquiry: {serializer.validated_data['subject']}",
-                message=serializer.validated_data['message'],
-                from_email=serializer.validated_data['email'],
-                recipient_list=['sekibaalamark44@gmail.com'],
+# 1. Fetch website content (GET)
+class CompanyInfoView(APIView):
+    def get(self, request):
+        # Get the first record (created by Admin)
+        profile = CompanyProfile.objects.first()
+        
+        if not profile:
+            return Response(
+                {"message": "No company profile found. Please create one in Admin."}, 
+                status=status.HTTP_404_NOT_FOUND
             )
-            return Response({"message": "Sent successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        serializer = CompanyProfileSerializer(profile, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
