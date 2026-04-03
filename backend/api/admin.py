35aa91f0bee63a8f81d7040abdba520cfc940e9a
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:22:17 +0300

    registered models in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
+# admin.py
 from django.contrib import admin
+from .models import CompanyProfile, ContactInquiry
 
-# Register your models here.
+admin.site.register(CompanyProfile)
+admin.site.register(ContactInquiry)
