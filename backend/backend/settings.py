a00afe0c9ecd07e8ead9049e12577f973c83700b
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:27:11 +0300

    configured cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     
+    'corsheaders',
     
     'rest_framework',
     
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
