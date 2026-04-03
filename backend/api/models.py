403578ce277ec69e3601e27325acc8b210370360
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:10:18 +0300

    created Contact inquiry model

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,3 +10,14 @@ class CompanyProfile(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+    
+
+
+class ContactInquiry(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
