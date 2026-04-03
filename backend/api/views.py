79313cc966b0718974ef37dad37479b2a252c165
sekibaalamark <markmajje256@gmail.com>
2026-04-03 16:21:30 +0300

    created contact create view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.mail import send_mail
+from .serializers import ContactInquirySerializer
 
-# Create your views here.
+class ContactCreateView(APIView):
+    def post(self, request):
+        serializer = ContactInquirySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            # Send Email
+            send_mail(
+                subject=f"New Inquiry: {serializer.validated_data['subject']}",
+                message=serializer.validated_data['message'],
+                from_email=serializer.validated_data['email'],
+                recipient_list=['sekibaalamark44@gmail.com'],
+            )
+            return Response({"message": "Sent successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
