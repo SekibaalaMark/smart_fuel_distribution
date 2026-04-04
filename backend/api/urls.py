af0f9c5cfa027c6266fe87b8a6fca4729dd820df
sekibaalamark <markmajje256@gmail.com>
2026-04-04 21:33:51 +0300

    added end points for admin

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import CompanyInfoView, ContactCreateView
+from .views import *
 
 urlpatterns = [
     path('company-info/', CompanyInfoView.as_view(), name='company-info'),
 
     path('contact/', ContactCreateView.as_view(), name='contact-us'),
+    
+    
+    
+    # ADMIN ENDPOINTS (Dashboard side)
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('admin/update-site/', AdminCompanyUpdateView.as_view()),
+    path('admin/inquiries/', AdminInquiryListView.as_view()),
+    path('admin/inquiries/<int:pk>/reply/', AdminReplyEmailView.as_view()),
 ]
