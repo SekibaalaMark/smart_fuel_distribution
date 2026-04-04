d2d4479bfc921107eb2dcc2971dc300db4a86e03
sekibaalamark <markmajje256@gmail.com>
2026-04-04 17:07:08 +0300

    added rest framework authentication classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,12 +118,12 @@ STATIC_URL = 'static/'
 
 
 
+# settings.py
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
-
 
 
 
