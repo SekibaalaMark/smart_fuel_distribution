6afa9b9d62084636f91c3c7b1a96c0ec477ee8d7
sekibaalamark <markmajje256@gmail.com>
2026-04-04 21:30:14 +0300

    customized the jwt token load

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,29 @@
 from rest_framework import serializers
-from .models import ContactInquiry, CompanyProfile
+from .models import *
+
+
+# serializers.py
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims into the JWT payload (optional)
+        token['username'] = user.username
+        token['is_superuser'] = user.is_superuser
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Add extra data to the JSON response body for the frontend
+        data['username'] = self.user.username
+        data['is_superuser'] = self.user.is_superuser
+        data['is_staff'] = self.user.is_staff
+        
+        return data
 
 class CompanyProfileSerializer(serializers.ModelSerializer):
     class Meta:
