59937ae18a52aac72983c7af5edbf8442dcd0d3c
sekibaalamark <markmajje256@gmail.com>
2026-04-09 16:05:26 +0300

    configured neon db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,8 @@
 
-
+import dj_database_url
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +75,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# This will look for a DATABASE_URL environment variable
+# If it doesn't find one (like on your local machine), it defaults to SQLite
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
